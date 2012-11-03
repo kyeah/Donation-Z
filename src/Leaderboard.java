712b2424643c7f39e23bf12ea27351de9d6337da
critterx <critterx@43729eb5-33c2-47f5-bada-55b66b79c8f2>
2012-11-03 08:00:03 +0000

    test

--- src/Leaderboard.java
+++ src/Leaderboard.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.FontFormatException;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
@@ -17,30 +18,47 @@ import donationz.ConnectDetails;
 public class Leaderboard
 {
 	static JFrame frame=Runner.frame;
+	static ArrayList<String> leader=new ArrayList<String>();
 	
-	public static void draw(Graphics g)
+	static
 	{
-		g.setColor(Color.black);
-		g.fillRect(0, 0, frame.getWidth(), frame.getHeight());
-		g.setColor(Color.white);
-		g.setFont(new Font("Arial",Font.PLAIN,24));
 		ConnectDetails connect = new ConnectDetails();
         String database = connect.getDBName();
         Connection con;
         try {
             Connect dz = new Connect(database);
             con = connect.getConnection();
-            ArrayList<String> leader=dz.viewScoreboard(con);
-            int i=0;
-            for(String s:leader)
-            {
-            	g.drawString(s, frame.getWidth()/2-100, 300+i*30);
-            	i++;
-            }
+            leader=dz.viewScoreboard(con);
+            System.out.println(leader);
         }
         catch(Exception e)
         {
         	e.getMessage();
+        }
+	}
+	
+	public static void draw(Graphics g)
+	{
+		g.setColor(Color.black);
+		g.fillRect(0, 0, frame.getWidth(), frame.getHeight());
+		g.setColor(Color.white);
+		try {
+			g.setFont(Font.createFont(Font.TRUETYPE_FONT, new File("resources"+File.separator+"fonts"+File.separator+"black.ttf")));
+			g.setFont(g.getFont().deriveFont(36f));
+		} catch (FontFormatException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		g.drawString("Current Leaders:", frame.getWidth()/2-100, 250);
+		int i=0;
+        for(String ss:leader)
+        {
+        	String arr[]=ss.split("\t");
+        	g.drawString(arr[0], frame.getWidth()/2-450, 300+i*30);
+        	g.drawString(arr[1], frame.getWidth()/2-200, 300+i*30);
+        	g.drawString(arr[2], frame.getWidth()/2+50, 300+i*30);
+        	i++;
         }
 	}
 
